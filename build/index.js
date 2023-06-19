4de770e9e4c9ee84fed3bf2712ddd7fe01e3a139
jaybee291998 <jaybee291998@gmail.com>
2023-06-19 11:01:13 +0800

    initial commit haha

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var greeting = '';
+for (var i = 0; i < 10; i++) {
+    greeting += 'za warudo ';
+}
+console.log(greeting);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let greeting = ''
+for (let i = 0; i < 10; i++) {
+    greeting += 'za warudo '
+}
+
+console.log(greeting)
